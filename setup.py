e5012116c0f4c13a14301ed62bc8b1660861d2aa
MMDRZA <info@mmdrza.com>
2025-07-02 20:59:30 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="libit",
-    version="5.1.8",
+    version="5.0.3",
     license="http://opensource.org/licenses/MIT",
     packages=find_packages(),
     install_requires=["pycryptodome>=3.15.0", "ecdsa>=0.18.0"],
